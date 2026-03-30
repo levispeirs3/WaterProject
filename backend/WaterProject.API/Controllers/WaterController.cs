9de2aa6f1da76bd31a0e9f49b304bc8cb0a51672
levispeirs3 <speirs03@byu.edu>
2026-03-30 11:49:29 -0600

    Initial project import

---
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using WaterProject.API.Data;
+
+namespace WaterProject.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WaterController : ControllerBase
+    {
+        private WaterDbContext _waterContext;
+
+        public WaterController(WaterDbContext temp)
+        {
+            _waterContext = temp;
+        }
+
+        [HttpGet("AllProjects")]
+        public IEnumerable<Project> Get()
+        {
+             var something =  _waterContext.Projects.ToList();
+             return something;
+        }
+
+        [HttpGet("FunctionalProjects")]
+        public IEnumerable<Project> GetFunctionalProjects()
+        {
+            var something = _waterContext.Projects.Where(x => x.ProjectFunctionalityStatus == "Functional").ToList();
+            return something;
+        }
+    }
+}
